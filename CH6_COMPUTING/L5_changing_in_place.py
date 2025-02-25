50e834e5eafd0facd73651015104f04d4053fadb
angelfarhat <devtahraf@gmail.com>
2025-02-25 15:42:55 -0600

    CH6:L5

---
+++ CH6_COMPUTING/L5_changing_in_place.py
@@ -0,0 +1,5 @@
+# Complete the update_player_score function. It should add increment to current_score and then return the new current_score.
+
+def update_player_score(current_score, increment):
+    current_score += increment
+    return current_score
