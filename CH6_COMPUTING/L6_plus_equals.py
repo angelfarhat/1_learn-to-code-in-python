e9102f8e7cced00265c513f279061e3d82618c75
angelfarhat <devtahraf@gmail.com>
2025-02-25 15:43:24 -0600

    CH6:L6

---
+++ CH6_COMPUTING/L6_plus_equals.py
@@ -0,0 +1,7 @@
+# Complete the get_hurt function. It should use the -= in-place operator to subtract damage from current_health and then return the new current_health.
+
+# Note: You cannot use -= in a return statement. Set the variable first, and then return it after!
+
+def get_hurt(current_health, damage):
+    current_health -= damage
+    return current_health
