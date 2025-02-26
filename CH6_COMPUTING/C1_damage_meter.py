08c870d69f60981edced48f964885b993454146d
angelfarhat <devtahraf@gmail.com>
2025-02-26 17:52:29 -0600

    CH6:C1

---
+++ CH6_COMPUTING/C1_damage_meter.py
@@ -0,0 +1,28 @@
+#Damage Meter
+#You've been working with some interns to build a damage meter for Fantasy Quest. They've added some calls to the main function that 
+#contain invalid syntax.
+
+#Assignment
+#Fix the intern's syntax error. The calculate_dps function takes two inputs, but due to the syntax error, it's taking in 4 instead. Use the proper #delimiter so that the numbers are still easy to parse visually.
+
+#The two input numbers should be:
+
+# damage: 8 million, time: 45
+# damage: 10 million, time: 49
+
+
+def main():
+    calculate_dps(8_000_000, 45)
+    calculate_dps(10_000_000, 49)
+
+
+# Don't edit below this line
+
+
+def calculate_dps(damage, time):
+    dps = damage / time
+    print(f"Damage per second: {dps}")
+    print("=====================================")
+
+
+main()
